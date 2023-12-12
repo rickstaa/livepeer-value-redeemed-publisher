334f5484a7e1860ca17f04c833263c10f4b49107
rickstaa <rick.staa@outlook.com>
2023-12-12 09:50:21 +0100

    refactor: improve code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Livepeer_value_redeemed publisher
 
-A simple Prometheus exporter that can be used to temporarily increment the `livepeer_value_redeemed` vector counter metric with a given value and publish it on http://localhost:7936/metrics. This can be used to correct the `livepeer_value_redeemed` metric due to a bug that was present in the `go-livepeer` binary before [go-livepeer/2916](https://github.com/livepeer/go-livepeer/pull/2916) was merged.
+A simple Prometheus exporter that can be used to temporarily increment the `livepeer_value_redeemed` vector counter metric with a given value and publish it on http://localhost:7935/metrics. This can be used to correct the `livepeer_value_redeemed` metric due to a bug that was present in the `go-livepeer` binary before [go-livepeer/2916](https://github.com/livepeer/go-livepeer/pull/2916) was merged.
 
 ## Usage
 
@@ -15,10 +15,13 @@ go build
 1. Run the [go-livepeer](https://github.com/livepeer/go-livepeer) binary without the `-monitor` flag.
 2. To increment the metric, Run the `livepeer_value_redeemed` binary with the `-value` and `-node_id` flags. The value should be in wei and represent the error in the `livepeer_value_redeemed` metric.
 
-   ```basg
+   ```bash
    ./livepeer_value_redeemed -value number -node_id string
    ```
 
 3. Wait for the value to be published.
 4. Stop the `livepeer_value_redeemed` binary.
 5. Start the [go-livepeer](https://github.com/livepeer/go-livepeer) binary with the `-monitor` flag.
+
+> [!NOTE]\
+> You can also change the `prometheus.yml` to listen for Livepeer metrics on a different port than the default `7935` port and then run the `livepeer_value_redeemed` binary with the `-port` flag.
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 // A simple Prometheus exporter that can be used to temporarily increment the `livepeer_value_redeemed`
-// vector counter metric with a given value and publish it on http://localhost:7936/metrics. This can
+// vector counter metric with a given value and publish it on http://localhost:7935/metrics. This can
 // be used to correct the `livepeer_value_redeemed` metric due to a bug that was present in the `go-livepeer`
 // binary before https://github.com/livepeer/go-livepeer/pull/2916 was merged.
 package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -16,48 +15,62 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+const WAIT_TIME = 60
+
 // Create `livepeer_value_redeemed` counter vector metric.
 var counterVec = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "livepeer_value_redeemed",
-		Help: "This counter shows the value redeemed",
+		Help: " Winning ticket value redeemed",
 	},
 	[]string{"node_id", "node_type"},
 )
 
 func main() {
+	log.Println("Starting `livepeer_value_redeemed` exporter...")
+
 	// Retrieve increment value and node ID from command line arguments.
 	valuePtr := flag.String("value", "", "Value to be published")
 	nodeIDPtr := flag.String("node_id", "", "Node ID")
+
+	// If -port is provided, use it to override the default port.
+	portPtr := flag.String("port", "7935", "Port to be used")
+
+	// Parse the command line arguments.
 	flag.Parse()
+
 	if *valuePtr == "" || *nodeIDPtr == "" {
 		log.Fatal("Both value and node_id must be provided.")
 	}
-	fmt.Println("Value to be published to livepeer_value_redeemed:", *valuePtr)
-	fmt.Println("Node ID label to be used:", *nodeIDPtr)
+	log.Println("Value published to `livepeer_value_redeemed` metric:", *valuePtr)
+	log.Println("Node ID label to be used:", *nodeIDPtr)
 
 	// Register counter vector metric and initialize it with a value of 0.
+	log.Println("Registering `livepeer_value_redeemed` counter vector metric with a value of 0...")
 	prometheus.MustRegister(counterVec)
 	counter := counterVec.With(prometheus.Labels{"node_id": *nodeIDPtr, "node_type": "orch"})
 	counter.Add(0)
 
-	// Create a goroutine that will increment the counter with the provided value after 5 seconds.
+	// Create a goroutine that will increment the counter with the provided value after 60 seconds.
 	go func() {
-		time.Sleep(5 * time.Second)
+		// Wait for 60 seconds.
+		log.Println("Waiting for 60 seconds...")
+		time.Sleep(WAIT_TIME * time.Second)
 
 		// Convert value to float64 and increment counter.
 		parsedValue, err := strconv.ParseFloat(*valuePtr, 64)
 		if err != nil {
-			log.Fatal("Failed to convert value to float64.")
+			log.Printf("Failed to convert value to float64: %v", err)
+			return
 		}
 		counter.Add(parsedValue)
-		log.Println("Value published to livepeer_value_redeemed:", *valuePtr)
+		log.Println("The `livepeer_value_redeemed` counter vector was incremented with", parsedValue)
 	}()
 
 	// Start HTTP server and expose metrics.
+	log.Println("Metric published on port:", *portPtr)
 	http.Handle("/metrics", promhttp.Handler())
-	err := http.ListenAndServe(":7936", nil)
-	if err != nil {
-		log.Fatal("Failed to start server: ", err)
+	if err := http.ListenAndServe(":"+*portPtr, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
